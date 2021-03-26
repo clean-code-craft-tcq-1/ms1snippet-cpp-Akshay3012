4bc96c4e51ecdf8957320ecbe24ae812709e94e4
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-03-26 16:46:33 +0530

    Removed Duplicacy.

--- sensor-validate.cpp
+++ sensor-validate.cpp
@@ -1,26 +1,30 @@
 #include "sensor-validate.h"
 
-bool isSensorReadingsHaveSuddenJumps_b(double value, double nextValue, double maxDelta) {
-  if(nextValue - value > maxDelta) {
+bool isSensorReadingsHaveSuddenJumps_b(double currentValue, double nextValue, double maxDelta) {
+  if(nextValue - currentValue > maxDelta) {
     return false;
   }
   return true;
 }
 
-bool validateSOCreadings_b(double* values, int numOfValues) {
-  int lastButOneIndex = numOfValues - 1;
-  for(int i = 0; i < lastButOneIndex; i++) {
-    if(!isSensorReadingsHaveSuddenJumps_b(values[i], values[i + 1], 0.05)) {
+bool validateSOCreadings_b(double* values, int numOfReadings) 
+{
+  for(int SOCReadingIndex = 0; SOCReadingIndex < (numOfReadings -1); SOCReadingIndex++) 
+  {
+    if(!isSensorReadingsHaveSuddenJumps_b(values[SOCReadingIndex], values[SOCReadingIndex + 1], 0.05)) 
+    {
       return false;
     }
   }
   return true;
 }
 
-bool validateCurrentreadings_b(double* values, int numOfValues) {
-  int lastButOneIndex = numOfValues - 1;
-  for(int j = 0; j < lastButOneIndex; j++) {
-    if(!isSensorReadingsHaveSuddenJumps_b(values[j], values[j + 1], 0.1)) {
+bool validateCurrentreadings_b(double* values, int numOfReadings) 
+{
+  for(int CurrentReadingIndex = 0; CurrentReadingIndex < (numOfReadings-1); CurrentReadingIndex++) 
+  {
+    if(!isSensorReadingsHaveSuddenJumps_b(values[CurrentReadingIndex], values[CurrentReadingIndex + 1], 0.1)) 
+    {
       return false;
     }
   }
