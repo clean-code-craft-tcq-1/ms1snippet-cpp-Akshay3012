864e47ef6ec1874febc5cb7b444826d53456c6f8
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-03-26 19:37:39 +0530

    Updated the Test cases in case of for Null Pointer

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -9,8 +9,20 @@ TEST_CASE("reports error when soc jumps abruptly") {
   REQUIRE(validateSOCreadings_b(socReadings, numOfSocReadings) == false);
 }
 
+TEST_CASE("reports error when there is no reading of soc") { 
+  double socReadings_empty[] = {};
+  int numOfSocReadings_empty = sizeof(socReadings_empty) / sizeof(socReadings_empty[0]);
+  REQUIRE(validateSOCreadings(socReadings_empty, numOfSocReadings_empty) == false);
+}
+
 TEST_CASE("reports error when current jumps abruptly") {
   double currentReadings[] = {0.03, 0.03, 0.03, 0.33};
   int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
   REQUIRE(validateSOCreadings_b(currentReadings, numOfCurReadings) == false);
+}
+
+TEST_CASE("reports error when there is no reading of current") { 
+  double currentReadings_empty[] = {};
+  int numOfCurReadings_empty = sizeof(currentReadings_empty) / sizeof(currentReadings_empty[0]);
+  REQUIRE(validateSOCreadings(currentReadings_empty, numOfCurReadings_empty) == false);
 }
