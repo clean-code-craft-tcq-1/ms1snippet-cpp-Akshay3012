ded84686d14e2f98a5256e49e95ad86782c59a52
Akshay3012 <77444334+Akshay3012@users.noreply.github.com>
2021-03-26 20:29:46 +0530

    Update sensor-validate.h

--- sensor-validate.h
+++ sensor-validate.h
@@ -1,6 +1,8 @@
 
 bool isSensorReadingsHaveSuddenJumps_b(double value, double nextValue, double maxDelta);
 
+bool getReadingStatus_b(double* values, int numOfReadings, double maxDelta);
+
 bool validateSOCreadings_b(double* values, int numOfValues);
 
 bool validateCurrentreadings_b(double* values, int numOfValues);
